1f482c682cc541cb222bd30cdd87931ca8566b2d
Nathan <nathanlakey9@gmail.com>
2019-12-04 17:56:27 -0600

    Added Semicolon's

--- booleanExercise.js
+++ booleanExercise.js
@@ -4,16 +4,16 @@ var johnWeight = 94
 var markHeight = 1.8
 var johnHeight = 2.1
 
-markBmi = markWeight / (markHeight * markHeight)
-console.log(markBmi)
-johnBmi = johnWeight / (johnHeight * johnHeight)
-console.log(johnBmi)
+markBmi = markWeight / (markHeight * markHeight);
+console.log(markBmi);
+johnBmi = johnWeight / (johnHeight * johnHeight);
+console.log(johnBmi);
 
  var markHigherBmi = markBmi > johnBmi
  var markHeavier = markWeight > johnWeight
  var markTaller = markHeight > johnHeight
 
- console.log('Does Mark have a higher BMI? ' + markHigherBmi + '. Is Mark heavier than John? ' + markHeavier + '. Is Mark taller than John? ' + markTaller + '.')
+ console.log('Does Mark have a higher BMI? ' + markHigherBmi + '. Is Mark heavier than John? ' + markHeavier + '. Is Mark taller than John? ' + markTaller + '.');
 
- alert('Mark\'s weight is ' + markWeight + 'kg. ' + ' John\'s weight is ' + johnWeight + 'kg.')
- console.log('Mark\'s height is ' + markHeight + ' meters.' + ' John\'s height is ' + johnHeight + ' meters.')
+ alert('Mark\'s weight is ' + markWeight + 'kg. ' + ' John\'s weight is ' + johnWeight + 'kg.');
+ console.log('Mark\'s height is ' + markHeight + ' meters.' + ' John\'s height is ' + johnHeight + ' meters.');
