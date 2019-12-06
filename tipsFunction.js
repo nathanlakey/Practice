bf6add75b86c537c80f8f56d21e1642588f6751d
Nathan <nathanlakey9@gmail.com>
2019-12-05 18:17:21 -0600

    Added a few more calculations and outputs

--- tipsFunction.js
+++ tipsFunction.js
@@ -12,13 +12,27 @@ function tipCalc(bill) {
 
 var tipList = [];
 var totalList = [];
-var tripTotal = 0; 
+var tripTotal = 0;
+var tipTotal = 0;
+var billTotal = 0
 
 for (i = 0; i < johnBills.length; i++) {
     tipList.push(tipCalc(johnBills[i]))
     totalList.push(tipCalc(johnBills[i]) + (johnBills[i]))
     tripTotal = tripTotal + (tipCalc(johnBills[i]) + (johnBills[i]))
+    tipTotal = tipTotal + (tipCalc(johnBills[i]))
+    billTotal = billTotal + (johnBills[i])
 };
+
+var avgPercent = tipTotal / billTotal;
+console.log('The average tip percentage was ' + avgPercent * 100 + '%!!!');
+
+var avgTip = tipTotal / johnBills.length;
+console.log('The average tip was $' + avgTip);
+
+var avgMeal = tripTotal / johnBills.length;
+console.log('The average meal price was $' + avgMeal);
+
 console.log(tipList, totalList, tripTotal);
 
 
